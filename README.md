eecf8d518e93d6b729e73c81de8e916bf6f22bf2
Nicholas Griffin <me@nicholasgriffin.co.uk>
2021-09-16 19:02:41 +0100

    First
    Just setting up some placeholder text :)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# nextjs-site-starter
- This is a starter template repo for Next.JS websites
+# Get the Gist?
+
+Welcome to my new app for taking and managing notes through the GitHub Gists service.
+
+This is just a side project from myself at the moment, it's really just a starter at this point, I'll be updating this soon :).
--- site/src/layout/Home/index.tsx
+++ site/src/layout/Home/index.tsx
@@ -9,19 +9,20 @@ export const Home = () => {
         <title>Nicholas Griffin - App Homepage</title>
       </Head>
       <Hero
-        title="Welcome to my new app!"
-        description="This is about all there is here for now, but I'm sure more content will be coming soon!"
+        title="Get the Gist?"
+        description="Welcome to my new app for taking and managing notes through the GitHub Gists service."
         link={{
           href: 'https://nicholasgriffin.dev',
           text: 'Find out more about me',
-          external: true
+          external: true,
         }}
       />
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
             <p className="font-semibold lg:w-2/3 mx-auto leading-relaxed title-font text-lg">
-              This is where I might place some content at some point...
+              This is just a side project from myself at the moment, it's really
+              just a starter at this point, I'll be updating this soon :).
             </p>
           </div>
         </div>
