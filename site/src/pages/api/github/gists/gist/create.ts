66e400bbe670242b26ad52d35b6a2d74e2a444f9
Nicholas Griffin <me@nicholasgriffin.co.uk>
2021-09-17 19:47:44 +0100

    feat: Adding and updating github apis

---
+++ site/src/pages/api/github/gists/gist/create.ts
@@ -0,0 +1,70 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { getSession } from 'next-auth/client';
+import { getToken } from 'next-auth/jwt';
+const { Octokit } = require('@octokit/rest');
+
+const api = async (req: NextApiRequest, res: NextApiResponse) => {
+  const secret = process.env.NEXT_JWT_AUTH_SECRET;
+  const token = await getToken({ req, secret });
+  const session = await getSession({ req });
+
+  const accessToken =
+    token && token.accessToken
+      ? token.accessToken
+      : session && session.accessToken
+      ? session.accessToken
+      : null;
+
+  if (accessToken) {
+    const octokit = new Octokit(
+      accessToken
+        ? {
+            auth: `token ${accessToken}`,
+          }
+        : {},
+    );
+
+    if (req.method === 'POST') {
+      if (!req.body.files) {
+        res.status(400).json({
+          status: 'Error',
+          message: 'No files were provided!',
+        });
+      }
+
+      octokit.rest.gists
+        .create({
+          files: req.body.files,
+          description: req.body.description || '',
+          public: req.body.public || false,
+        })
+        .then(({ data }) => {
+          res.status(200).json({
+            status: 'Success',
+            message: 'Retrieved!',
+            data,
+          });
+        })
+        .catch(err => {
+          console.error(err);
+          res.status(500).json({
+            status: 'Error',
+            message: 'Something went wrong!',
+          });
+        });
+    } else {
+      res.status(405).json({
+        status: 'Error',
+        message: 'Method Not Allowed',
+      });
+    }
+  } else {
+    res.status(400).json({
+      status: 'Error',
+      message: 'You must be signed in to use this API!',
+    });
+  }
+};
+
+export default api;
